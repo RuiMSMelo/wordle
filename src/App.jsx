95cc954dede1a240daaff091a4b42aed7e4d5324
RuiMSMelo <ruimsmelo@gmail.com>
2025-07-24 12:46:19 +0100

    fetch done and data saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [word, setWord] = useState('')
 
-    return <>hello</>
+    useEffect(() => {
+        fetch('https://random-word-api.herokuapp.com/word')
+            .then((response) => {
+                console.log('response', response)
+                return response.json()
+            })
+            .then((data) => {
+                console.log('data', data)
+                setWord(data)
+            })
+    }, [])
+
+    return <>Wordle: {word}</>
 }
 
 export default App
