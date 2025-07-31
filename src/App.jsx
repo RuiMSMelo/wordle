e77f9785734677e6fc1b4f80516d072409a6a9d6
RuiMSMelo <ruimsmelo@gmail.com>
2025-07-31 12:14:34 +0100

    changing api

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import './App.css'
 function App() {
     const [word, setWord] = useState('')
 
-    useEffect(() => {
-        fetch('https://random-word-api.herokuapp.com/word')
-            .then((response) => {
-                console.log('response', response)
-                return response.json()
-            })
-            .then((data) => {
-                console.log('data', data)
-                setWord(data)
-            })
-    }, [])
+    // useEffect(() => {
+    //     fetch('https://random-word-api.herokuapp.com/word')
+    //         .then((response) => {
+    //             console.log(response.statusText)
+    //             return response.json()
+    //         })
+    //         .then((data) => {
+    //             console.log('data', data)
+    //             setWord(data)
+    //         })
+    // }, [])
 
-    return <>{word ? word : 'fetching...'}</>
+    // return <>{word ? word : 'fetching...'}</>
+
+    const website = 'https://wordle-api.vercel.app/api/wordle'
 }
 
 export default App
